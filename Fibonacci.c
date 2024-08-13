be743d99203c0ab172889e4d9fdd333815ccdcd5
nany625 <jimmy19980625@gmail.com>
2024-08-13 21:38:19 +0800

    Create Fibonacci.c

---
+++ Fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_FIB_SIZE 10000
+
+typedef struct {
+    char *number;
+    short digits;
+} Fib;
+
+Fib fib[MAX_FIB_SIZE + 1];
+
+void initFibonacci() {
+    fib[0].number = strdup("0");
+    fib[1].number = strdup("1");
+    fib[0].digits = fib[1].digits = 1;
+    for(int i = 2; i <= MAX_FIB_SIZE; ++i) {
+        fib[i].number = (char*)malloc((fib[i - 1].digits + 1) * sizeof(char));
+        memset(fib[i].number, '0', fib[i - 1].digits + 1);
+        short j;
+        for(j = 0; j < fib[i - 1].digits; ++j) {
+        	fib[i].number[j + 1] += (fib[i].number[j] + fib[i - 1].number[j] + fib[i - 2].number[j] - 3 * '0') / 10;
+        	fib[i].number[j] = (fib[i].number[j] + fib[i - 1].number[j] + fib[i - 2].number[j] - 3 * '0') % 10 + '0';
+        }
+        fib[i].digits = j + (fib[i].number[j] - '0');
+    }
+}
+
+int main() {
+    initFibonacci();
+    int n;
+    while(scanf("%d", &n) == 1) {
+        printf("The Fibonacci number for %d is ", n);
+        for(int i = fib[n].digits - 1; i >= 0; --i)
+            putchar(fib[n].number[i]);
+        puts(".");
+    }
+    for(int i = 0; i <= MAX_FIB_SIZE; ++i)
+        free(fib[i].number);
+    return 0;
+}
