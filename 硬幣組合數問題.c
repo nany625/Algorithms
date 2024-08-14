c345fac8f5efc218ee1422ecdc015336b9098fce
nany625 <jimmy19980625@gmail.com>
2024-08-14 14:18:47 +0800

    Create 硬幣組合數問題.c

---
+++ 硬幣組合數問題.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *coins = NULL, size = 0, coin;
+    while(scanf("%d", &coin) && coin != 0) {
+        coins = (int*)realloc(coins, (size + 1) * sizeof(int));
+        coins[size++] = coin;
+    }
+    int amount;
+    while(scanf("%d", &amount) == 1) {
+        int dp[amount + 1];
+        dp[0] = 1;
+        for(int i = 1; i <= amount; ++i)
+            dp[i] = 0;
+        for(int i = 0; i < size; ++i) {
+            for(int j = coins[i]; j <= amount; ++j)
+                dp[j] += dp[j - coins[i]];
+        }
+        printf("%d\n", dp[amount]);
+    }
+    free(coins);
+    return 0;
+}
