26c224fe4f242518f5b382c4c8799eb86ede4043
nany625 <jimmy19980625@gmail.com>
2024-08-15 00:14:56 +0800

    Update 完全背包問題.c

--- 完全背包問題.c
+++ 完全背包問題.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    int weight, value;
+} Item;
+
 int main() {
-    
-    return 0;
+	Item *items = NULL;
+	int size = 0, w, v;
+	while(scanf("%d %d", &w, &v) && w != 0 && v != 0) {
+	    items = (Item*)realloc(items, (size + 1) * sizeof(Item));
+	    items[size].weight = w;
+	    items[size++].value = v;
+	}
+	int capacity;
+	scanf("%d", &capacity);
+	int dp[capacity + 1];
+	for(int i = 0; i <= capacity; ++i)
+	    dp[i] = 0;
+	for(int i = 0; i < size; ++i) {
+	    w = items[i].weight;
+	    v = items[i].value;
+	    for(int j = w; j <= capacity; ++j)
+	        dp[j] = dp[j] > dp[j - w] + v ? dp[j] : dp[j - w] + v;
+	}
+	printf("Maximum value in Complete Knapsack: %d\n", dp[capacity]);
+	free(items);
+	return 0;
 }
