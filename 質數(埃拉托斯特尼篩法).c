a76aa0ad19cd0e4349180f29e06a2058e94c893b
nany625 <jimmy19980625@gmail.com>
2024-08-14 13:29:14 +0800

    Create 質數(埃拉托斯特尼篩法).c

---
+++ 質數(埃拉托斯特尼篩法).c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#define MAX_NUM 1000000
+
+bool isComposite[MAX_NUM + 1] = {true, true};
+int *primes = NULL, primeCount;
+
+void sieve() {
+    primeCount = 0;
+    int limit = sqrt(MAX_NUM);
+    for(int n = 2; n <= MAX_NUM; ++n) {
+        if(!isComposite[n]) {
+            primes = (int*)realloc(primes, (primeCount + 1) * sizeof(int));
+            primes[primeCount++] = n;
+            if(n <= limit) {
+                for(int i = n * n; i <= MAX_NUM; i += n)
+                    isComposite[i] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    sieve();
+    int n;
+    while(scanf("%d", &n) && n != 0)
+        printf("%d is %sprime.\n", n, !isComposite[n] ? "" : "not ");
+    free(primes);
+    return 0;
+}
