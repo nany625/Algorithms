3f7a30e2fd835920ebbb8fa4e134c422434d594f
nany625 <jimmy19980625@gmail.com>
2024-08-17 22:51:12 +0800

    Update 階乘.c

--- 階乘.c
+++ 階乘.c
@@ -1,3 +1,4 @@
+// #解法一
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -45,10 +46,10 @@ int main() {
     return 0;
 }
 
+// #解法二
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 #define MAX_FACTORIAL_SIZE 1000
 
 typedef struct {
@@ -57,16 +58,15 @@ typedef struct {
 } BigNumber;
 
 BigNumber fac[MAX_FACTORIAL_SIZE + 1];
+char temp[5];
 
 void initFactorial() {
 	fac[0].number = strdup("0");
 	fac[1].number = strdup("1");
     fac[0].digits = fac[1].digits = 1;
-    char *temp = NULL;
     for(int i = 2; i <= MAX_FACTORIAL_SIZE; ++i) {
-        int len = log10(i) + 1;
-        temp = (char*)malloc((len + 1) * sizeof(char));
         sprintf(temp, "%d", i);
+        int len = strlen(temp);
         fac[i].number = (char*)malloc((fac[i - 1].digits + len + 1) * sizeof(char));
         memset(fac[i].number, '0', (fac[i - 1].digits + len) * sizeof(char));
         fac[i].number[fac[i - 1].digits + len] = '\0';
@@ -76,13 +76,10 @@ void initFactorial() {
                 fac[i].number[j + k + 1] = ((fac[i - 1].number[k] - '0') * (temp[j] - '0') + fac[i].number[j + k + 1] - '0') % 10 + '0';
             }
         }
-        int j = 0;
-        while(fac[i].number[j] == '0')
-            ++j;
-        fac[i].digits = fac[i - 1].digits + len - j;
-        memmove(fac[i].number, fac[i].number + j, fac[i].digits * sizeof(char));
+        fac[i].digits = fac[i - 1].digits + len - (fac[i].number[0] == '0');
+        if(fac[i].number[0] == '0')
+            memmove(fac[i].number, fac[i].number + 1, fac[i].digits * sizeof(char));
         fac[i].number[fac[i].digits] = '\0';
-        free(temp);
     }
 }
 
