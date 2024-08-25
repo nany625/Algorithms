b85194b019adb69ccb3914b90f43f24cba182b3e
nany625 <jimmy19980625@gmail.com>
2024-08-25 17:52:37 +0800

    Update 費波那契數列.c

--- 費波那契數列.c
+++ 費波那契數列.c
@@ -39,3 +39,31 @@ int main() {
         free(fib[i].number);
     return 0;
 }
+
+/*
+Input:
+0
+1
+2
+3
+4
+5
+10
+20
+50
+100
+1000
+
+Output:
+The Fibonacci number for 0 is 0
+The Fibonacci number for 1 is 1
+The Fibonacci number for 2 is 1
+The Fibonacci number for 3 is 2
+The Fibonacci number for 4 is 3
+The Fibonacci number for 5 is 5
+The Fibonacci number for 10 is 55
+The Fibonacci number for 20 is 6765
+The Fibonacci number for 50 is 12586269025
+The Fibonacci number for 100 is 354224848179261915075
+The Fibonacci number for 1000 is 43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875
+*/
