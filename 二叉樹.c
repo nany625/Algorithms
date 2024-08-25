fde7f153def94ffafba9ce371ed8e069abf2179b
nany625 <jimmy19980625@gmail.com>
2024-08-25 17:42:54 +0800

    Update 二叉樹.c

--- 二叉樹.c
+++ 二叉樹.c
@@ -26,14 +26,14 @@ TreeNode *insertNode(TreeNode *root, int data) {
 void inorderTraversal(TreeNode *root) {
     if(root) {
         inorderTraversal(root->left);
-        printf("%d ", root->data);
+        printf(" %d", root->data);
         inorderTraversal(root->right);
     }
 }
 
 void preorderTraversal(TreeNode *root) {
     if(root) {
-        printf("%d ", root->data);
+        printf(" %d", root->data);
         preorderTraversal(root->left);
         preorderTraversal(root->right);
     }
@@ -43,7 +43,7 @@ void postorderTraversal(TreeNode *root) {
     if(root) {
         postorderTraversal(root->left);
         postorderTraversal(root->right);
-        printf("%d ", root->data);
+        printf(" %d", root->data);
     }
 }
 
@@ -60,13 +60,42 @@ int main() {
     int n;
     while(scanf("%d", &n) == 1)
         root = insertNode(root, n);
-    fputs("Inorder Traversal: ", stdout);
+    fputs("Inorder Traversal:", stdout);
     inorderTraversal(root);
-    fputs("\nPreorder Traversal: ", stdout);
+    fputs("\nPreorder Traversal:", stdout);
     preorderTraversal(root);
-    fputs("\nPostorder Traversal: ", stdout);
+    fputs("\nPostorder Traversal:", stdout);
     postorderTraversal(root);
     putchar('\n');
     freeTree(root);
     return 0;
 }
+
+/*
+Input:
+50
+30
+60
+40
+80
+20
+90
+70
+10
+
+Output:
+Inorder Traversal: 10 20 30 40 50 60 70 80 90
+Preorder Traversal: 50 30 20 10 40 60 80 70 90
+Postorder Traversal: 10 20 40 30 70 90 80 60 50
+*/
+
+/*
+Binary Search Tree:
+         50
+        /  \
+      30    60
+     /  \     \
+   20    40    80
+  /           /  \
+10          70    90
+*/
