8f048d6bd41a29d61247ef488a0f5bcdeb56e1f8
nany625 <jimmy19980625@gmail.com>
2024-10-20 11:11:39 +0800

    Update 分數背包問題.c

--- 分數背包問題.c
+++ 分數背包問題.c
@@ -12,27 +12,24 @@ int compare(const void *a, const void *b) {
 }
 
 int main() {
-	Item *items = NULL;
-	int size = 0, w, v;
-	while(scanf("%d %d", &w, &v) && w != 0 && v != 0) {
-	    items = (Item*)realloc(items, (size + 1) * sizeof(Item));
-	    items[size].weight = w;
-	    items[size++].value = v;
-	}
-	qsort(items, size, sizeof(Item), compare);
-	int capacity;
-	scanf("%d", &capacity);
+    int n;
+    scanf("%d", &n);
+	Item items[n];
+	for(int i = 0; i < n; ++i)
+	    scanf("%d %d", &items[i].weight, &items[i].value);
+	qsort(items, n, sizeof(Item), compare);
+	int W;
+	scanf("%d", &W);
 	double maxValue = 0;
-	for(int i = 0; i < size; ++i) {
-	    if(capacity >= items[i].weight) {
+	for(int i = 0; i < n; ++i) {
+	    if(W >= items[i].weight) {
 	        maxValue += items[i].value;
-	        capacity -= items[i].weight;
+	        W -= items[i].weight;
 	    } else {
-	        maxValue += capacity * (double)items[i].value / items[i].weight;
+	        maxValue += W * (double)items[i].value / items[i].weight;
 	        break;
 	    }
 	}
 	printf("Maximum value in Fractional Knapsack: %.2lf\n", maxValue);
-	free(items);
 	return 0;
 }
