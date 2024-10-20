b0ffe882c8b057466c2d1468ae7a655557b045a0
nany625 <jimmy19980625@gmail.com>
2024-10-20 15:26:52 +0800

    Create 質數(埃拉托斯特尼篩法).java

---
+++ 質數(埃拉托斯特尼篩法).java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int MAX_NUM = 1000000;
+    static boolean[] isComposite = new boolean[MAX_NUM + 1];
+	static ArrayList<Integer> primes = new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+	    sieve();
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(br);
+        StringBuilder output = new StringBuilder();
+        int n;
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0)
+            output.append(n).append(" is ").append(!isComposite[n] ? "prime\n" : "not prime\n");
+        System.out.print(output);
+	}
+	
+	static void sieve() {
+	    isComposite[0] = isComposite[1] = true;
+        int limit = (int)Math.sqrt(MAX_NUM);
+        for(int n = 2; n <= MAX_NUM; ++n) {
+            if(!isComposite[n]) {
+                primes.add(n);
+                if(n <= limit) {
+                    for(int i = n * n; i <= MAX_NUM; i += n)
+                        isComposite[i] = true;
+                }
+            }
+        }
+    }
+}
