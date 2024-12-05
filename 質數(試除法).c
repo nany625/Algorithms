68cae501a9adb6a33de8277ec5efcd45c32ec473
nany625 <jimmy19980625@gmail.com>
2024-12-05 16:56:06 +0800

    Update 質數(試除法).c

--- 質數(試除法).c
+++ 質數(試除法).c
@@ -4,11 +4,11 @@
 #include <stdbool.h>
 #define MAX_NUM 1000000
 
-int *primes, primeCount;
+int *primes, size;
 
 bool isPrime(int n) {
     int limit = sqrt(n);
-    for(int i = 0; i < primeCount && primes[i] <= limit; ++i) {
+    for(int i = 0; i < size && primes[i] <= limit; ++i) {
         if(n % primes[i] == 0)
             return false;
     }
@@ -18,11 +18,11 @@ bool isPrime(int n) {
 void primeTrailDivision() {
     primes = (int*)malloc(sizeof(int));
     primes[0] = 2;
-    primeCount = 1;
+    size = 1;
     for(int n = 3; n <= MAX_NUM; n += 2) {
         if(isPrime(n)) {
-            primes = (int*)realloc(primes, (primeCount + 1) * sizeof(int));
-            primes[primeCount++] = n;
+            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
+            primes[size++] = n;
         }
     }
 }
