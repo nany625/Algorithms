5426e99760ae3cf22479f9f2044da090052aa3d8
nany625 <jimmy19980625@gmail.com>
2024-12-05 16:53:28 +0800

    Update 質數(埃拉托斯特尼篩法).c

--- 質數(埃拉托斯特尼篩法).c
+++ 質數(埃拉托斯特尼篩法).c
@@ -5,14 +5,14 @@
 #define MAX_NUM 1000000
 
 bool isComposite[MAX_NUM + 1] = {true, true};
-int *primes, primeCount;
+int *primes, size;
 
 void eratosthenesSieve() {
     int limit = sqrt(MAX_NUM);
     for(int n = 2; n <= MAX_NUM; ++n) {
         if(!isComposite[n]) {
-            primes = (int*)realloc(primes, (primeCount + 1) * sizeof(int));
-            primes[primeCount++] = n;
+            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
+            primes[size++] = n;
             if(n <= limit) {
                 for(int i = n * n; i <= MAX_NUM; i += n)
                     isComposite[i] = true;
