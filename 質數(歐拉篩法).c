a5606e489ef955ef0157b10e99b5a04d53640b8b
nany625 <jimmy19980625@gmail.com>
2024-12-06 11:26:20 +0800

    Update 質數(歐拉篩法).c

--- 質數(歐拉篩法).c
+++ 質數(歐拉篩法).c
@@ -8,7 +8,7 @@ int *primes, size;
 
 void eulerSieve() {
     for(int n = 2; n <= MAX_NUM; ++n) {
-        if(!isComposite[i]) {
+        if(!isComposite[n]) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
