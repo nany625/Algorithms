e8b2f4662405e48baa7a726352396c4cabbf93aa
nany625 <jimmy19980625@gmail.com>
2024-12-06 14:09:26 +0800

    Update 質數(埃拉托斯特尼篩法).java

--- 質數(埃拉托斯特尼篩法).java
+++ 質數(埃拉托斯特尼篩法).java
@@ -1,10 +1,8 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     static int MAX_NUM = 1000000;
     static boolean[] isComposite = new boolean[MAX_NUM + 1];
-	static ArrayList<Integer> primes = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
 	    eratosthenesSieve();
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,12 +18,9 @@ public class Main {
 	    isComposite[0] = isComposite[1] = true;
         int limit = (int)Math.sqrt(MAX_NUM);
         for(int n = 2; n <= MAX_NUM; ++n) {
-            if(!isComposite[n]) {
-                primes.add(n);
-                if(n <= limit) {
-                    for(int i = n * n; i <= MAX_NUM; i += n)
-                        isComposite[i] = true;
-                }
+            if(!isComposite[n] && n <= limit) {
+                for(int i = n * n; i <= MAX_NUM; i += n)
+                    isComposite[i] = true;
             }
         }
     }
