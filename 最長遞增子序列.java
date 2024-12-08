8e6e118512a3dcfe0f49c32c9ead43e59f43b0e5
nany625 <jimmy19980625@gmail.com>
2024-12-08 12:48:01 +0800

    Update 最長遞增子序列.java

--- 最長遞增子序列.java
+++ 最長遞增子序列.java
@@ -9,13 +9,13 @@ public class Main {
         int n;
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0)
             array.add(n);
-        System.out.println("Length of LIS of " + array.toString() + " is " + lengthOfLIS(array, array.size()));
+        System.out.println("Length of LIS of " + array.toString() + " is " + lengthOfLIS(array));
 	}
 	
-	static int binarySearch(ArrayList<Integer> array, int arraySize, int key) {
-        int left = 0, right = arraySize - 1;
+	static int binarySearch(ArrayList<Integer> array, int key) {
+        int left = 0, right = array.size() - 1;
         while(left <= right) {
-            int mid = left + ((right - left) >> 1);
+            int mid = left + (right - left >> 1);
             if(array.get(mid) < key)
                 left = mid + 1;
             else
@@ -24,17 +24,16 @@ public class Main {
         return left;
     }
     
-    static int lengthOfLIS(ArrayList<Integer> array, int arraySize) {
+    static int lengthOfLIS(ArrayList<Integer> array) {
         ArrayList<Integer> LIS = new ArrayList<>();
         int length = 0;
         for(Integer i : array) {
-            int pos = binarySearch(LIS, length, i);
-            if(pos == length) {
+            int pos = binarySearch(LIS, i);
+            if(pos == LIS.size())
                 LIS.add(i);
-                ++length;
-            } else
+            else
                 LIS.set(pos, i);
         }
-        return length;
+        return LIS.size();
     }
 }
