d26f52cd656e6f77d37dfb37f81c434a11c91ad7
nany625 <jimmy19980625@gmail.com>
2024-12-08 12:54:16 +0800

    Update 逆序數對.java

--- 逆序數對.java
+++ 逆序數對.java
@@ -16,7 +16,7 @@ public class Main {
 	
 	static void MergeSort(ArrayList<Integer> array, int[] reg, int left, int right, long[] swap) {
         if(left < right) {
-            int mid = left + ((right - left) >> 1);
+            int mid = left + (right - left >> 1);
             MergeSort(array, reg, left, mid, swap);
             MergeSort(array, reg, mid + 1, right, swap);
             int leftPointer = left, rightPointer = mid + 1;
