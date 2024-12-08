63dddac100e28212f6e93b5d3e811121ba544891
nany625 <jimmy19980625@gmail.com>
2024-12-08 12:41:33 +0800

    Update 最大子數列.java

--- 最大子數列.java
+++ 最大子數列.java
@@ -9,12 +9,12 @@ public class Main {
         int n;
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0)
             array.add(n);
-        System.out.println("Max subarray sum of " + array.toString() + ": " + maxSubArraySum(array, array.size()));
+        System.out.println("Max subarray sum of " + array.toString() + ": " + maxSubArraySum(array));
 	}
 	
-	static int maxSubArraySum(ArrayList<Integer> array, int arraySize) {
+	static int maxSubArraySum(ArrayList<Integer> array) {
         int max = array.get(0), curr = array.get(0);
-        for(int i = 1; i < arraySize; ++i) {
+        for(int i = 1; i < array.size(); ++i) {
             curr = Math.max(array.get(i), curr + array.get(i));
             max = Math.max(max, curr);
         }
