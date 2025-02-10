81df99963c726e269b3fa881be432f831495a9c4
nany625 <jimmy19980625@gmail.com>
2025-02-10 13:44:38 +0800

    Update and rename 單源最短路徑(Dijkstra演算法).c to 單源最短路徑(Dijkstra 演算法).c

--- 單源最短路徑(Dijkstra 演算法).c
+++ 單源最短路徑(Dijkstra 演算法).c
@@ -88,3 +88,79 @@ int main() {
     free(graph);
     return 0;
 }
+
+/*
+Input:
+9 14
+0 1 4
+0 7 8
+1 2 8
+1 7 11
+2 3 7
+2 5 4
+2 8 2
+3 4 9
+3 5 14
+5 4 10
+6 5 2
+6 8 6
+7 6 1
+7 8 7
+0
+1
+2
+
+Output:
+Shorted distance from node 0 to other nodes:
+to node 0: 0
+0
+to node 1: 4
+0 -> 1
+to node 2: 12
+0 -> 1 -> 2
+to node 3: 19
+0 -> 1 -> 2 -> 3
+to node 4: 21
+0 -> 7 -> 6 -> 5 -> 4
+to node 5: 11
+0 -> 7 -> 6 -> 5
+to node 6: 9
+0 -> 7 -> 6
+to node 7: 8
+0 -> 7
+to node 8: 14
+0 -> 1 -> 2 -> 8
+Shorted distance from node 1 to other nodes:
+to node 0: impossible
+to node 1: 0
+1
+to node 2: 8
+1 -> 2
+to node 3: 15
+1 -> 2 -> 3
+to node 4: 22
+1 -> 2 -> 5 -> 4
+to node 5: 12
+1 -> 2 -> 5
+to node 6: 12
+1 -> 7 -> 6
+to node 7: 11
+1 -> 7
+to node 8: 10
+1 -> 2 -> 8
+Shorted distance from node 2 to other nodes:
+to node 0: impossible
+to node 1: impossible
+to node 2: 0
+2
+to node 3: 7
+2 -> 3
+to node 4: 14
+2 -> 5 -> 4
+to node 5: 4
+2 -> 5
+to node 6: impossible
+to node 7: impossible
+to node 8: 2
+2 -> 8
+*/
