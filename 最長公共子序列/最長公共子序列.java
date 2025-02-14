926ecc54a93103cd025387c2e7ff445fa06d31c0
nany625 <jimmy19980625@gmail.com>
2025-02-14 18:12:29 +0800

    Create 最長公共子序列.java

---
+++ 最長公共子序列/最長公共子序列.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(br);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_WORD) {
+            String X = st.sval;
+            st.nextToken();
+            String Y = st.sval;
+            output.append("Length of LCS of strings \"").append(X).append("\" and \"").append(Y).append("\": ").append(lengthOfLCS(X, Y)).append('\n');
+        }
+        System.out.print(output);
+	}
+	
+	static int lengthOfLCS(String X, String Y) {
+        int[] LCS = new int[Y.length() + 1];
+        for(int i = 0; i < X.length(); ++i) {
+            int prev = 0;
+            for(int j = 1; j <= Y.length(); ++j) {
+                int temp = LCS[j];
+                if(X.charAt(i) == Y.charAt(j - 1))
+                    LCS[j] = prev + 1;
+                else
+                    LCS[j] = Math.max(LCS[j], LCS[j - 1]);
+                prev = temp;
+            }
+        }
+        return LCS[Y.length()];
+    }
+}
