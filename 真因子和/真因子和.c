438b8e7b9149ef6afa4ffda701fef2a65c84b99d
nany625 <jimmy19980625@gmail.com>
2025-02-14 17:58:13 +0800

    Create 真因子和.c

---
+++ 真因子和/真因子和.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#define MAX_NUM 1000000
+
+bool isComposite[MAX_NUM + 1];
+int *primes, size;
+
+void eulerSieve() {
+    for(int n = 2; n <= MAX_NUM; ++n) {
+        if(!isComposite[n]) {
+            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
+            primes[size++] = n;
+        }
+        for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+            isComposite[temp] = true;
+            if(n % primes[i] == 0)
+                break;
+        }
+    }
+}
+
+long trueFactorSum(long n) {
+    long sum = 1, limit = sqrt(n), temp = n;
+    for(int i = 0; i < size && primes[i] <= limit; ++i) {
+        if(n % primes[i] == 0) {
+            long tempSum = 1, term = 1;
+            do {
+                tempSum += term *= primes[i];
+                n /= primes[i];
+            } while(n % primes[i] == 0);
+            sum *= tempSum;
+            limit = sqrt(n);
+        }
+    }
+    if(n > 1)
+        sum *= (1 + n);
+    return sum - temp;
+}
+
+int main() {
+    eulerSieve();
+    long n;
+    while(scanf("%ld", &n) && n != 0)
+        printf("Sum of true factors of %ld: %ld\n", n, trueFactorSum(n));
+    free(primes);
+    return 0;
+}
