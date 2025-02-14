f11e07b71b1fbc7420daad5cf8db07a471a41408
nany625 <jimmy19980625@gmail.com>
2025-02-14 17:45:18 +0800

    Create 泡沫排序法.c

---
+++ 排序/泡沫排序法.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void BubbleSort(int *array, int arraySize) {
+    bool sorted = false;
+    for(int i = 0; i < arraySize - 1 && !sorted; ++i) {
+        sorted = true;
+        for(int j = 0; j < arraySize - 1 - i; ++j) {
+            if(array[j] > array[j + 1]) {
+                swap(&array[j], &array[j + 1]);
+                sorted = false;
+            }
+        }
+    }
+}
+
+int main() {
+    int *array = NULL, arraySize = 0, n;
+    while(scanf("%d", &n) == 1) {
+        array = (int*)realloc(array, (arraySize + 1) * sizeof(int));
+        array[arraySize++] = n;
+    }
+    BubbleSort(array, arraySize);
+    for(int i = 0; i < arraySize - 1; ++i)
+        printf("%d, ", array[i]);
+    printf("%d\n", array[arraySize - 1]);
+    free(array);
+    return 0;
+}
