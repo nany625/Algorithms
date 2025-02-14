f1b00f6c1b664e381e8fb41320d30930d1b4663e
nany625 <jimmy19980625@gmail.com>
2025-02-14 18:06:58 +0800

    Create 最大公因數.c

---
+++ 最大公因數/最大公因數.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+long GCD(long a, long b) {
+    return b == 0 ? a : GCD(b, a % b);
+}
+
+int main() {
+    long a, b;
+    while(scanf("%ld %ld", &a, &b) && a != 0 && b != 0)
+        printf("GCD of %ld and %ld: %ld\n", a, b, GCD(a, b));
+    return 0;
+}
