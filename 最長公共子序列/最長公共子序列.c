24f3941f409746faa79b1f2ba76eb4f9d6e451e9
nany625 <jimmy19980625@gmail.com>
2025-02-14 18:12:08 +0800

    Create 最長公共子序列.c

---
+++ 最長公共子序列/最長公共子序列.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int lengthOfLCS(char *X, char *Y) {
+    int m = strlen(X), n = strlen(Y);
+    int LCS[n + 1];
+    memset(LCS, 0, sizeof(LCS));
+    for(int i = 0; i < m; ++i) {
+        int prev = 0;
+        for(int j = 1; j <= n; ++j) {
+            int temp = LCS[j];
+            if(X[i] == Y[j - 1])
+                LCS[j] = prev + 1;
+            else
+                LCS[j] = LCS[j] > LCS[j - 1] ? LCS[j] : LCS[j - 1];
+            prev = temp;
+        }
+    }
+    return LCS[n];
+}
+
+int main() {
+    char *X = NULL, *Y = NULL;
+    while(scanf("%ms %ms", &X, &Y) == 2) {
+        printf("Length of LCS of strings \"%s\" and \"%s\": %d\n", X, Y, lengthOfLCS(X, Y));
+        free(X);
+        free(Y);
+        X = Y = NULL;
+    }
+    return 0;
+}
