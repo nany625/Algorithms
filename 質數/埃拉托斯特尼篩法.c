08a802676b78a950e1a10af19b5ef75b307c35a6
nany625 <jimmy19980625@gmail.com>
2025-02-14 17:32:01 +0800

    Create 埃拉托斯特尼篩法.c

---
+++ 質數/埃拉托斯特尼篩法.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#define MAX_NUM 1000000
+
+bool isComposite[MAX_NUM + 1] = {true, true};
+
+void eratosthenesSieve() {
+    int limit = sqrt(MAX_NUM);
+    for(int n = 2; n <= MAX_NUM; ++n) {
+        if(n <= limit && !isComposite[n]) {
+            for(int i = n * n; i <= MAX_NUM; i += n)
+                isComposite[i] = true;
+        }
+    }
+}
+
+int main() {
+    eratosthenesSieve();
+    int n;
+    while(scanf("%d", &n) && n != 0)
+        printf("%d is %sprime\n", n, !isComposite[n] ? "" : "not ");
+    return 0;
+}
