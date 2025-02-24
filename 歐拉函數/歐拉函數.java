8b1c6f0da854c571a75233663dd3d7540d714b5c
nany625 <jimmy19980625@gmail.com>
2025-02-24 16:02:36 +0800

    Update 歐拉函數.java

--- 歐拉函數/歐拉函數.java
+++ 歐拉函數/歐拉函數.java
@@ -1,3 +1,4 @@
+// #解法一
 import java.io.*;
 import java.util.*;
 
@@ -45,5 +46,34 @@ public class main {
         if(n > 1)
             ans -= ans / n;
         return ans;
+    }
+}
+
+// #解法二
+import java.io.*;
+
+public class main {
+    static int MAX_NUM = 1000000;
+    static int[] phi = new int[MAX_NUM + 1];
+	public static void main(String[] args) throws IOException {
+	    for(int n = 2; n <= MAX_NUM; ++n)
+            phi[n] = n;
+	    eratosthenesSieve();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
+		int n;
+		StringBuilder output = new StringBuilder();
+		while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0)
+			output.append("φ(").append(n).append(") = ").append(phi[n]).append('\n');    // 小於等於正整數n的所有與n互質的正整數個數
+		System.out.print(output);
+	}
+    
+    static void eratosthenesSieve() {
+        for(int n = 2; n <= MAX_NUM; ++n) {
+            if(phi[n] == n) {
+                for(int i = n; i <= MAX_NUM; i += n)
+                    phi[i] -= phi[i] / n;
+            }
+        }
     }
 }
