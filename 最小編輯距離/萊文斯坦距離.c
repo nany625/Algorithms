0b37cf7460b6e4295383076edea629f929dee5e2
nany625 <jimmy19980625@gmail.com>
2025-02-26 19:20:39 +0800

    Update 萊文斯坦距離.c

--- 最小編輯距離/萊文斯坦距離.c
+++ 最小編輯距離/萊文斯坦距離.c
@@ -1,43 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
-#define MIN3(a, b, c) MIN(MIN(a, b), c)
+int min(int a, int b, int c) {
+    b = b < a ? b : a;
+    return c < b ? c : b;
+}
 
-int levenshtein_distance(const char *s1, const char *s2) {
-    int len1 = strlen(s1), len2 = strlen(s2);
-    int dp[len2 + 1];
-
-    // 初始化 base case：dp[0][j] = j
-    for (int j = 0; j <= len2; j++) {
-        dp[j] = j;
-    }
-
-    for (int i = 1; i <= len1; i++) {
-        int prev = dp[0];  // `dp[i-1][0]`
-        dp[0] = i;  // `dp[i][0]` = i
-        
-        for (int j = 1; j <= len2; j++) {
-            int temp = dp[j];  // 保存 `dp[i-1][j]`
-            
-            if (s1[i - 1] == s2[j - 1]) {
-                dp[j] = prev;  // 若字元相同，取 `dp[i-1][j-1]`
-            } else {
-                dp[j] = 1 + MIN3(dp[j],      // 刪除 s1[i-1]
-                                  dp[j - 1],  // 插入 s2[j-1]
-                                  prev);      // 替換 s1[i-1] -> s2[j-1]
-            }
-            
-            prev = temp;  // 更新 `prev` 為 `dp[i-1][j]`
+int levenshteinDistance(char *X, char *Y) {
+    int m = strlen(X), n = strlen(Y);
+    int dp[n + 1];
+    for(int i = 0; i <= n; ++i)
+        dp[i] = i;
+    for(int i = 1; i <= m; ++i) {
+        int prev = dp[0];
+        dp[0] = i;
+        for(int j = 1; j <= n; ++j) {
+            int temp = dp[j];
+            if(X[i - 1] == Y[j - 1])
+                dp[j] = prev;
+            else
+                dp[j] = 1 + min(temp, dp[j - 1], prev);
+            prev = temp;
         }
     }
-
-    return dp[len2];
+    return dp[n];
 }
 
 int main() {
-    char s1[] = "kitten";
-    char s2[] = "sitting";
-    printf("Levenshtein Distance: %d\n", levenshtein_distance(s1, s2));
-    return 0;
+    char *X = NULL, *Y = NULL;
+    while(scanf("%ms %ms", &X, &Y) == 2) {
+        printf("Levenshtein Distance of strings \"%s\" and \"%s\": %d\n", X, Y, levenshteinDistance(X, Y));
+        free(X);
+        free(Y);
+        X = Y = NULL;
+    }
+	return 0;
 }
