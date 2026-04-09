b5057e9cdc0ce8142805cc4e88bcb56e2b9c9338
nany625 <jimmy19980625@gmail.com>
2026-04-09 18:36:47 +0800

    Update 埃拉托斯特尼篩法.java

--- 質數/埃拉托斯特尼篩法.java
+++ 質數/埃拉托斯特尼篩法.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main {
     static int MAX_NUM = 1000000;
-    static boolean[] isComposite = new boolean[MAX_NUM + 1];
+    static int[] mark = new int[(MAX_NUM >> 5) + 1];
 	public static void main(String[] args) throws IOException {
 	    eratosthenesSieve();
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,17 +10,26 @@ public class Main {
         StringBuilder output = new StringBuilder();
         int n;
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0)
-            output.append(n).append(!isComposite[n] ? " is prime\n" : " is not prime\n");
+            output.append(n).append(!GET(n) ? " is prime\n" : " is not prime\n");
         System.out.print(output);
 	}
+
+    static boolean GET(int n) {
+	    return (mark[n >> 5] & (1 << (n & 31))) != 0;
+	}
 	
+	static void SET(int n) {
+	    mark[n >> 5] |= 1 << (n & 31);
+	}
+    
 	static void eratosthenesSieve() {
-	    isComposite[0] = isComposite[1] = true;
+	    SET(0);
+        SET(1);
         int limit = (int)Math.sqrt(MAX_NUM);
         for(int n = 2; n <= MAX_NUM; ++n) {
-            if(n <= limit && !isComposite[n]) {
+            if(n <= limit && !GET(n)) {
                 for(int i = n * n; i <= MAX_NUM; i += n)
-                    isComposite[i] = true;
+                    SET(i);
             }
         }
     }
