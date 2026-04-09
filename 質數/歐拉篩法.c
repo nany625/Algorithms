f947f0260b701f6efd1b6a407ef64fa0536ac6a6
nany625 <jimmy19980625@gmail.com>
2026-04-09 18:16:06 +0800

    Update 歐拉篩法.c

--- 質數/歐拉篩法.c
+++ 質數/歐拉篩法.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #define MAX_NUM 1000000
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[MAX_NUM + 1] = {true, true};
-int *primes, size;
+unsigned int mark[(MAX_NUM >> 6) + 1];
+int *primes, size = 1;
 
 void eulerSieve() {
-    for(int n = 2; n <= MAX_NUM; ++n) {
-        if(!isComposite[n]) {
+    SET(0);
+    primes = (int*)realloc(primes, sizeof(int));
+    primes[0] = 2;
+    for(int n = 3; n <= MAX_NUM; n += 2) {
+        if(!GET(n >> 1)) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
-        for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp] = true;
+        for(int i = 1, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
+            SET(temp >> 1);
             if(n % primes[i] == 0)
                 break;
         }
@@ -24,7 +28,7 @@ int main() {
     eulerSieve();
     int n;
     while(scanf("%d", &n) && n != 0)
-        printf("%d is %sprime\n", n, !isComposite[n] ? "" : "not ");
+        printf("%d is %sprime\n", n, n == 2 || (n & 1 && !GET(n >> 1)) ? "" : "not ");
     free(primes);
     return 0;
 }
