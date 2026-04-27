10c7e2aeee7a5505072f53e878767b3183d3bac4
nany625 <jimmy19980625@gmail.com>
2026-04-27 11:42:22 +0800

    Update 最長遞增子序列(最早出現).cpp

--- 最長遞增子序列/最長遞增子序列(最早出現).cpp
+++ 最長遞增子序列/最長遞增子序列(最早出現).cpp
@@ -38,3 +38,12 @@ int main() {
     cout << LIS.back() << "]\n";
     return 0;
 }
+
+/*
+Input:
+-7 10 9 2 3 8 8 6
+
+Output:
+Length of LIS of [-7, 10, 9, 2, 3, 8, 8, 6] is 4
+The first occurrence LIS is [-7, 2, 3, 8]
+*/
